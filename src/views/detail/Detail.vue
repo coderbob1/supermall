692f51d10e6689414eb81407c6bc4e6f16cf4d2e
coderbob1 <631624480@qq.com>
2020-11-16 17:41:55 +0800

    更新

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="detail">
-    <detail-nav-bar class="detail-nav"/>
-    <scroll class="content" ref="scroll">
+    <detail-nav-bar class="detail-nav" @titleClick="titleClick" ref="nav"/>
+    <scroll class="content" ref="scroll" @scroll="contentScroll" :probe-type="3">
       <detail-swiper :top-images="topImages"/>
       <detail-base-info :goods="goods"/>
       <detail-shop-info :shop="shop"/>
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
-      <detail-param-info :param-info="paramInfo"/>
+      <detail-param-info ref="param" :param-info="paramInfo"/>
+      <detail-comment-info ref="comment" :comment-info="commentInfo"/>
+      <goods-list ref="recommend" :goods="recommends"/>
     </scroll>
   </div>
 </template>
@@ -18,10 +20,13 @@
   import DetailShopInfo from './childComps/DetailShopInfo'
   import DetailGoodsInfo from './childComps/DetailGoodsInfo'
   import DetailParamInfo from './childComps/DetailParamInfo'
+  import DetailCommentInfo from './childComps/DetailCommentInfo'
 
   import Scroll from 'components/common/scroll/Scroll'
+  import GoodsList from "components/content/goods/GoodsList";
 
-  import {getDetail, Goods, Shop, GoodsParam} from "network/detail";
+  import {getDetail, Goods, Shop, GoodsParam, getRecommend} from "network/detail";
+  import {debounce} from "@/common/utils";
 
   export default {
     name: "Detail",
@@ -32,7 +37,9 @@
       DetailShopInfo,
       DetailGoodsInfo,
       DetailParamInfo,
-      Scroll
+      Scroll,
+      DetailCommentInfo,
+      GoodsList
     },
     data() {
       return {
@@ -41,7 +48,12 @@
         goods: {},
         shop: {},
         detailInfo: {},
-        paramInfo: {}
+        paramInfo: {},
+        commentInfo:{},
+        themeTopYs:[],
+        recommends:[],
+        getThemeTopY:null,
+        currentIndex:0
       }
     },
     created() {
@@ -66,11 +78,46 @@
 
         // 5.获取参数的信息
         this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+
+        // 6.获取评论的信息
+        if (data.rate.cRate !== 0){
+          this.commentInfo = data.rate.list[0]
+        }
       })
+
+      getRecommend().then(res=>{
+        console.log(res);
+        this.recommends = res.data.list
+      })
+
+      this.getThemeTopY = debounce(()=>{
+        this.themeTopYs.push(0);
+        this.themeTopYs.push(this.$refs.param.$el.offsetTop);
+        this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+        this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+      })
+    },
+    mounted() {
+
     },
     methods: {
       imageLoad() {
         this.$refs.scroll.refresh()
+        this.getThemeTopY()
+      },
+      titleClick(index){
+        this.$refs.scroll.scrollTo(0,-this.themeTopYs[index], 200)
+      },
+      contentScroll(position){
+        const positionY = -position.y;
+        const length = this.themeTopYs.length;
+        for (let i = 0;i<length;i++) {
+          if (this.currentIndex !== i && ((i < length -1 && positionY>this.themeTopYs[i] && positionY < this.themeTopYs[i+1]) || (i === length -1 && positionY>this.themeTopYs[i] ))){
+            this.currentIndex = i ;
+            // console.log(this.currentIndex);
+            this.$refs.nav.currentIndex  = this.currentIndex;
+          }
+        }
       }
     }
   }
